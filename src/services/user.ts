8b18c38e4440f84cae4ea672636237167593a3dc
fladioarmandika <fladioarmandika@gmail.com>
2021-04-01 14:50:48 +0700

    ✨ add user service

---
+++ src/services/user.ts
@@ -0,0 +1,24 @@
+import User, { UserDocument } from "../models/user";
+
+export default class UserService {
+
+    public createUser(userParams: UserDocument, callback: any) {
+        const session = new User(userParams);
+        session.save();
+    }
+
+    public getUser(query: any, callback: any) {
+        User.findOne(query, callback);
+    }
+
+    public deleteUser(userId: string, callback: any) {
+        const query = { _id: userId }
+        User.deleteOne(query, callback);
+    }
+
+    public updateUser(userId: string, updated: any, callback: any) {
+        const query = { _id: userId }
+        User.findOneAndUpdate(query, updated, callback);
+    }
+
+}
